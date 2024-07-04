b75473cdf788e4fea8a355fa766e79fbaf6fd9d7
Oculux <nate.williamson.nz@gmail.com>
2024-07-04 16:49:49 +1200

    :sparkles: add test comment data + route

---
+++ backend/src/routes/comments/testData.js
@@ -0,0 +1,14 @@
+export const testComments = [
+  {
+    id: 1,
+    message: "This is a test comment.",
+  },
+  {
+    id: 2,
+    message: "This is another test comment.",
+  },
+  {
+    id: 3,
+    message: "This is a third test comment.",
+  },
+];
