88ac403813a8775877cf02ca0f7519285a3fa485
zlrkw11 <ray040406@gmail.com>
2024-07-04 15:48:10 +1200

    styling the window

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-
+import "98.css"
+import styles from './app.module.css'
 function App() {
   
 
   return (
    
-      <div>
-
+      <div className={`window ${styles.mainWindow} `}>
+          ray
       </div>
 
     
