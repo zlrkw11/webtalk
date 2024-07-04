716ec5b4826ad69b396315f2efb71be341c9567f
zlrkw11 <ray040406@gmail.com>
2024-07-04 16:00:53 +1200

    title bar

--- frontend/src/Window.jsx
+++ frontend/src/Window.jsx
@@ -3,13 +3,15 @@ import styles from './app.module.css'
 const Window = () => {
     return (
     <div className={`window ${styles.windowTitle} `}>
+        <div className={`title-bar`}>
         <div className={`title-bar-text`}>web talk</div>
           <div className="title-bar-controls">
             <button aria-label="Minimize" />
             <button aria-label="Maximize" />
             <button aria-label="Close" />
           </div>
-          <div className={`window-body ${styles.windowBody}`}>ray</div>
+        </div>
+        <div className={`window-body ${styles.windowBody}`}>ray</div>
       </div>
       
     )
